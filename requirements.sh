2c826b9fb6bc6696ca6538b77135cdcd44b9e81d
xxalejann323xx <82472529+xxalejann323xx@users.noreply.github.com>
2022-05-02 20:08:01 +0200

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,17 @@
+#Dependencias
+
+dependencias() {
+
+	clear
+	printf "\e[1;93mInstalando dependencias\e[0m\n"
+	printf "\n\e[1;91mXterm\e[0m\n"
+	sudo apt-get install -y xterm
+	printf "\n\e[1;34mAircrack-ng\e[0m\n"
+	sudo apt-get install aircrack-ng
+	printf "\n\e[1;92mWifite\e[0m\n"
+	sudo apt-get install wifite
+	printf "\n\e[1;96mMDK3\e[0m\n"
+	sudo apt-get install mdk3
+}
+
+dependencias
